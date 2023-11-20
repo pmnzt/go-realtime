8403ae6cce5d18f66db0a69826d54222647220ef
isaac qadri <isaac@debian.isaac>
2023-11-20 19:15:20 +0300

    allow origin from anywhere

--- client.go
+++ client.go
@@ -35,6 +35,11 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// origin := r.Header.Get("Origin")
+		// return origin == "http://localhost:3000"
+		return true
+	},
 }
 
 // Client is a middleman between the websocket connection and the hub.
